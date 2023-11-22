80be3857540f6c1382bab6e5cc455250572e7bb6
FluxMusic <alex.huber743@gmail.com>
2023-11-22 22:53:55 +0100

    added glow around the falling stars

--- scripts/cursor-follower.js
+++ scripts/cursor-follower.js
@@ -9,8 +9,8 @@ const config = {
     timeBetweenStars: 25,
     animationDuration: 1500,
     animations: ['fall1', 'fall2', 'fall3'],
-    colors: ['rgb(3, 240, 240)', 'rgb(113, 254, 145)', 'rgb(254, 254, 245)', 'rgb(255, 207, 96)'],
-    sizes: ['1.5rem', '0.7rem', '1.3rem']
+    colors: ['rgb(3, 240, 240)', 'rgb(113, 254, 145)', 'rgb(254, 254, 245)', 'rgb(19, 203, 224)'],
+    sizes: ['1.5', '0.7', '1.3']
     // colors: ['invert(89%) sepia(16%) saturate(6837%) hue-rotate(121deg) brightness(96%) contrast(98%)',
     // 'invert(14%) sepia(70%) saturate(3121%) hue-rotate(266deg) brightness(93%) contrast(126%)',
     // 'invert(94%) sepia(7%) saturate(2263%) hue-rotate(358deg) brightness(106%) contrast(103%)']
@@ -52,19 +52,22 @@ function createPoint(position) {
 }
 function createStar(position) {
     const star = document.createElementNS('http://www.w3.org/2000/svg', "svg");
-    
+    const color = config.colors[selectRandom(config.colors)];
+    const size = config.sizes[selectRandom(config.sizes)];
+
     star.setAttribute('class', 'star');
 
     star.src = '/img/svg/star.svg'
 
     star.setAttribute('viewBox', '0 0 256 256');
-    star.setAttribute('fill', config.colors[selectRandom(config.colors)]);
+    star.setAttribute('fill', color);
 
     star.innerHTML = '<path d="M240.58984,128a15.84794,15.84794,0,0,1-10.53125,15.03711l-63.81543,23.206-23.206,63.81543a16.001,16.001,0,0,1-30.07422,0L89.75684,166.24316l-63.81543-23.206a16.001,16.001,0,0,1,0-30.07422L89.75684,89.75684l23.20605-63.81543a16.001,16.001,0,0,1,30.07422,0l23.206,63.81543,63.81543,23.20605A15.84794,15.84794,0,0,1,240.58984,128Z"/>';
     
     star.style.left = `${position.x}px`;
     star.style.top = `${position.y}px`;
-    star.style.height = config.sizes[selectRandom(config.sizes)];
+    star.style.filter = `drop-shadow(0px 0px ${size/2}rem ${color})`;
+    star.style.height = `${size}rem`;
     star.style.animationName = config.animations[count++ % 3];
     star.style.animationDuration = `${config.animationDuration}ms`;
     
