1cc335b0d183c326c60fed52b0d19aef9f3a80a2
FluxMusic <alex.huber743@gmail.com>
2023-11-24 00:39:12 +0100

    changed the star svg to text, made trail smoother

--- scripts/cursor-follower.js
+++ scripts/cursor-follower.js
@@ -14,29 +14,29 @@ const last = {
 };
 
 const config = {
-    glowSpacing: 5,
+    glowSpacing: 10,
     timeBetweenStars: 200,
     distanceBetweenStars: 75,
-    animationDuration: 1250,
+    animationDuration: 1000,
     animations: ['fall1', 'fall2', 'fall3'],
     colors: ['rgb(3, 240, 240)', 'rgb(113, 254, 145)', 'rgb(254, 254, 245)', 'rgb(19, 203, 224)'],
-    sizes: ['1.5', '0.7', '1.3']
+    sizes: ['1.7', '0.9', '1.5', '1.2']
 };
 
 let count = 0;
 
 document.getElementById('spielfeld').onmousemove = e => {
-    const x = e.layerX;
-    const y = e.layerY;
 
     const position = {
-        x: x,
-        y: y
+        x: e.layerX,
+        y: e.layerY
     };
 
+    console.log(position);
+
     const now = new Date().getTime();
-    const wasLongEnough = calcTime(last.start, now) >= config.timeBetweenStars;
     const movedFarEnough = calcDistance(last.starOrigin, position) >= config.distanceBetweenStars;
+    const wasLongEnough = calcTime(last.start, now) >= config.timeBetweenStars;
 
     createPoint(position);
 
@@ -46,29 +46,29 @@ document.getElementById('spielfeld').onmousemove = e => {
     }
 
     createTail(last.mouseOrigin, position);
-
+    
     updateLastMousePosition(position);
 }
 
 function createPoint(position) {
     const Point = document.createElement("div");
-
+    
     Point.className = 'glow-point';
-
+    
     Point.style.left = `${position.x}px`;
     Point.style.top = `${position.y}px`;
-
+    
     appendElement(Point);
-
-    removeElement(Point, 50);
+    
+    removeElement(Point, 60);
 }
 function createTail(lastPosition, position) {
     const distance = calcDistance(lastPosition, position);
     const amount = determinePointAmount(distance);
-
+    
     const dx = (position.x - lastPosition.x) / amount;
     const dy = (position.y - lastPosition.y) / amount;
-
+    
     for (let i = 0; i < amount; i++) {
         const x = lastPosition.x + dx * i;
         const y = lastPosition.y + dy * i;
@@ -76,29 +76,25 @@ function createTail(lastPosition, position) {
     }
 }
 function createStar(position) {
-    const star = document.createElementNS('http://www.w3.org/2000/svg', "svg");
-    const color = config.colors[selectRandom(config.colors)];
-    const size = config.sizes[selectRandom(config.sizes)];
+   const star = document.createElement('span');
+   const color = config.colors[selectRandom(config.colors)];
+   const size = config.sizes[selectRandom(config.sizes)];
 
-    star.setAttribute('class', 'star');
-
-    star.src = '/img/svg/star.svg'
-
-    star.setAttribute('viewBox', '0 0 256 256');
-    star.setAttribute('fill', color);
-
-    star.innerHTML = '<path d="M240.58984,128a15.84794,15.84794,0,0,1-10.53125,15.03711l-63.81543,23.206-23.206,63.81543a16.001,16.001,0,0,1-30.07422,0L89.75684,166.24316l-63.81543-23.206a16.001,16.001,0,0,1,0-30.07422L89.75684,89.75684l23.20605-63.81543a16.001,16.001,0,0,1,30.07422,0l23.206,63.81543,63.81543,23.20605A15.84794,15.84794,0,0,1,240.58984,128Z"/>';
+   star.innerHTML = '&#x2726;';
     
-    star.style.left = `${position.x}px`;
-    star.style.top = `${position.y}px`;
-    star.style.filter = `drop-shadow(0px 0px ${size/2}rem ${color})`;
-    star.style.height = `${size}rem`;
-    star.style.animationName = config.animations[count++ % 3];
-    star.style.animationDuration = `${config.animationDuration}ms`;
+   star.className = 'star';
+
+   star.style.color = color;
+   star.style.textShadow = `0px 0px ${size}rem ${color}`;
+   star.style.left = `${position.x}px`;
+   star.style.top = `${position.y}px`;
+   star.style.fontSize = `${size}rem`;
+   star.style.animationName = config.animations[count++ % 3];
+   star.style.animationDuration = `${config.animationDuration}ms`;
     
     appendElement(star);
-
-    removeElement(star, config.animationDuration - 10);
+    
+    removeElement(star, config.animationDuration);
 }
 function removeElement(element, delay) {
     setTimeout(() => gameField.removeChild(element), delay);
